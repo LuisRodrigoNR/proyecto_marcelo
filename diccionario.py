66d82fb41307baa82418b8d71c265e0c50f583fc
FMM28 <103226884+FMM28@users.noreply.github.com>
2023-11-14 21:09:11 -0600

    diccionario

---
+++ diccionario.py
@@ -0,0 +1,73 @@
+import wikipediaapi                 #pip install wikipedia-api
+import re
+import unidecode                    #pip install unidecode
+import matplotlib.pyplot as plt     #pip install matplotlib
+from collections import Counter
+
+#sacare palabras de una pagina de wikipedia utilzando su API
+
+def consulta(pagina,idioma):
+
+    user_agent ="diccionario (https://github.com/FMM28/diccionario)"
+    wiki_wiki = wikipediaapi.Wikipedia(user_agent=user_agent,language=idioma)
+
+    page = wiki_wiki.page(pagina)
+
+    datos = page.text.split()
+    diccionario=[]
+
+    for i in datos:
+        i=i.lower()
+        #utilizo la libreria re para poder eliminar signos de puntuacion y despues la libreria unicode para eliminar los acentos
+        i=unidecode.unidecode(re.sub(r'[^A-Za-záéíóúüñÁÉÍÓÚÜÑ]','',i))
+
+        # if not i in diccionario:
+        diccionario.append(i)
+    return diccionario
+
+def grafica(lista,idioma):
+    letras = ''.join(lista)                             #Junta todas las palabras de la lista en una sola
+    frecuencias = Counter(letras)                       #Cuenta la frequencia con la que se repite cada letra
+    letras_ordenadas = frecuencias.most_common()        #Ordena las letras
+    letras, frecuencias = zip(*letras_ordenadas)        #Separa las letras de las frecuencias para graficarlas
+    
+    plt.bar(letras, frecuencias)                        #Grafica
+    plt.xlabel('Letras')
+    plt.ylabel('Frecuencia')
+    plt.title('Frecuencia de Letras '+idioma)
+    plt.show()
+
+dicEng=consulta('YouTube','en')
+dicEng.extend(consulta('Facebook','en'))
+dicEng.extend(consulta('Wikipedia','en'))
+
+dicFr=consulta('YouTube','fr')
+dicFr.extend(consulta('Facebook','fr'))
+dicFr.extend(consulta('Wikipedia','fr'))
+
+dicAle=consulta('YouTube','de')
+dicAle.extend(consulta('Facebook','de'))
+dicAle.extend(consulta('Wikipedia','de'))
+
+dicPor=consulta('YouTube','pt')
+dicPor.extend(consulta('Facebook','pt'))
+dicPor.extend(consulta('Wikipedia','pt'))
+
+dicIt=consulta('YouTube','it')
+dicIt.extend(consulta('Facebook','it'))
+dicIt.extend(consulta('Wikipedia','it'))
+
+print('ingles',len(dicEng))
+print('frances',len(dicFr))
+print('aleman',len(dicAle))
+print('portugues',len(dicPor))
+print('italiano',len(dicIt))
+
+grafica(dicEng,'ingles')
+grafica(dicFr,'frances')
+grafica(dicAle,'aleman')
+grafica(dicPor,'portugues')
+grafica(dicIt,'italiano')
+
+# grafica('Jigcg xet onug e Uovnjcwfen xio mottgttgq jig fotj xonqgclvb Dootg wov uen afedang, loc gpgcw qew xign ig patajgq jig ngtj, jig Dootg ieq beaq e ygevjalvb, dbajjgcand, dobqgn gdd. Jig Uovnjcwfen jooh jig gddt jo fechgj enq toon ygden jo dgj caui. Yvj aj xet noj bond yglocg ig dcgx afmejagnj xaji jig Dootg yguevtg tig depg iaf onbw e tandbg dobqgn gdd e qew. Ig xet noj dgjjand caui letj gnovdi. Jign ong qew, eljgc ig ieq lanatigq uovnjand iat fongw, jig aqge uefg jo iaf jiej ig uovbq dgj ebb jig dobqgn gddt ej onug yw habband jig Dootg enq uvjjand aj omgn. Yvj xign jig qggq xet qong, noj e tandbg dobqgn gdd qaq ig lanq, enq iat mcguaovt Dootg xet qgeq'.replace('.','').replace(',','').replace(' ',''),'texto 1')
+# grafica('Tp sparnq oj Ijq jrd ebpqoj. At h p ijpzgnzk oj kajgjr. Pz bjf-oj-gcpzrrjj, at h p zq rptnq pljg zq kapqn, zqj rpttj-p-spqejb, zqj gzaraqj rkpgajzrj jd vpzqj jd zqj rpttj oj ipaq. At p wpad gnqrdbzabj jq ktzr zqj ijttj ljbpqop gnqdjqpqd zqj iaitandcjxzj, knzb kbnwadjb oz rntjat jd o’zqj lzj rzb tj vpboaq. Pz rnzr-rnt, at h p zqj gplj, knzb bpqejb tj laq. Pz kbjsajb jdpej, at h p dbnar gcpsibjr. Pz ojzuajsj jdpej, at h p zq izbjpz jd ojzu gcpsibjr, onqd tp rajqqj. Opqr tj izbjpz, at h p zq ljtn o’pkkpbdjsjqd, ojr cptdjbjr jd zq rdjkkjb. Tj vpboaq jrd tnqe jd jdbnad, at h p zqj iptpqçnabj pz wnqo jd zq ipbijgzj, kbngcj oj tp djbbprrj, rzb tpxzjttj jrd knrjj zq dbpqrpd. Ojr gcpdr lajqqjqd rnzljqd gcprrjb tjr wjzattjr xza lntjqd pz ljqd'.replace('.','').replace(',','').replace(' ',''),'texto 2')
